261e96c498d4c93d2068cc21c6142ec42fb21351
koperatti <koperatti@gmail.com>
2020-05-17 16:11:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -76,7 +76,7 @@ def time_format_check(date):
 
 def hash_replace(task,strings):
 	idx = strings.find(r'#')
-	result = src[:idx] + str(task) + src[idx+len(task):]
+	result = strings[:idx] + str(task) + strings[idx+len(task):]
 	return result
 # ↓コマンドの解釈をする関数
 def list_process(message):
