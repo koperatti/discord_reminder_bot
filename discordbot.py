830e1024e3ae729d0239472cb2337001b56acdd4
koperatti <koperatti@gmail.com>
2020-05-20 11:51:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,19 +9,19 @@ import asyncio
 import unicodedata
 import pprint
 
-TOKEN = os.environ['DISCORD_BOT_TOKEN'] # discord botのトークン。Heroku上で環境変数として設定している。
+TOKEN = os.environ['DISCORD_BOT_TOKEN']  # discord botのトークン。Heroku上で環境変数として設定している。
 DIFF_JST_FROM_UTC = 9
-started_time = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC) #起動した瞬間の時刻(JTC)を取得
-BOT_LOG_CHANNEL = 710813437675962449 # BotlogチャンネルのID
-BOT_COMMAND_CHANNEL = 710752335781036073 # コマンド送信用チャンネルのID
-BOT_DATA_CHANNEL = 710752335781036073 # 課題、イベント一覧チャンネルのID
+started_time = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)  #起動した瞬間の時刻(JTC)を取得
+BOT_LOG_CHANNEL = 710813437675962449  # BotlogチャンネルのID
+BOT_COMMAND_CHANNEL = 710752335781036073  # コマンド送信用チャンネルのID
+BOT_DATA_CHANNEL = 710752335781036073  # 課題、イベント一覧チャンネルのID
 remind_list = []
 day_later = 0
 on_cmd_cnl = False
 cmd_chl = True
 change = False
 task = ''
-# 以下のリストたちはbotのランダムな返信リスト。 #の部分がタスク名に置き換わる(No_hashだけはそのまま)
+# 以下のリストたちはbotのランダムな返信リスト。 '#'の部分がタスク名に置き換わる(No_hashだけはそのまま)
 Format_error_deadline =['ブブー！締切の日時のフォーマットが違います',
 			'ワタシ、ソノニチジ ワカラナイデス',
 			'残念！締切の日時のフォーマットエラーだ！',
@@ -60,7 +60,7 @@ Not_found = ['404エラー！この意味が分かるかな? #がいないって
 	     '# は迷子だ！見つからないよ！',
 	     '#?そんなやついたっけな?',
 	     'リスト中に#なし、を検出しました']
-Same_name = [#ならもうここにおるぞ！さては偽物だな！',
+Same_name = ['#ならもうここにおるぞ！さては偽物だな！',
 	     'どうやらあなたは課題リストをよく見ていないようだねぇ、#はとっくに登録済みだよ',
 	     'あれぇ?#?聴き覚えのある名前だなぁ']
 Rescheduled = ['ガチャコン！ #の締め切りが変わった！']
@@ -126,11 +126,14 @@ def time_format_check(date):
 		else:
 			date = 'Format error'
 	return date
+
+
 # rtn_msgの中の'#'をタスク名に置き換える関数
-def hash_replace(task,strings):
+def hash_replace(task, strings):
 	idx = strings.find(r'#')
-	result = strings[:idx] + str(task) + strings[idx+1:]
+	result = strings[:idx] + str(task) + strings[idx + 1:]
 	return result
+
 
 # 文字列の長さをスペースで保管する関数
 def left(digit, msg):
@@ -146,8 +149,9 @@ def left(digit, msg):
 	msg = msg + ' ' * digit
 	return msg
 
+
 # 二次元配列をそろえて表示するためのコマンド
-def list_show(remind_list, option = ['normal']):
+def list_show(remind_list, option=['normal']):
 	global day_later
 	sndmsg = ''
 	remind_list = sorted(remind_list)
@@ -198,6 +202,7 @@ def list_show(remind_list, option = ['normal']):
 			sndmsg = sndmsg + str(a[2]) + ')\n'
 	return sndmsg
 
+
 # ↓コマンドの解釈をする関数。
 def list_process(message, on_cmd_cnl):
 	global remind_list
@@ -208,14 +213,13 @@ def list_process(message, on_cmd_cnl):
 	cmd_chl = True
 	change = False
 	rtn_msg = ''
-	log_msg = ''
-	command = message.content # 入力されたメッセージを変数(command)に代入
-	if '/add' in command: # /add がメッセージ内に入っているかの判別
+	command = message.content   # 入力されたメッセージを変数(command)に代入
+	if '/add' in command:  # /add がメッセージ内に入っているかの判別
 		if on_cmd_cnl:
-			if '*' not in command: # * がメッセージ内に入っていないかの判別
-				if '#' not in command: # '#' がメッセージ内に入っていないかの判別
-					command_list = command.split()[1:] # コマンドをスペースで区切り、/addだけ消す(例：/add task subject 2020-08-28_12:34 → task,subject,2020-08-28_12:34)
-					if len(command_list) == 3: # コマンドが正しい形かどうか判別
+			if '*' not in command:  # * がメッセージ内に入っていないかの判別
+				if '#' not in command:  # '#' がメッセージ内に入っていないかの判別
+					command_list = command.split()[1:]  # コマンドをスペースで区切り、/addだけ消す(例：/add task subject 2020-08-28_12:34 → task,subject,2020-08-28_12:34)
+					if len(command_list) == 3:  # コマンドが正しい形かどうか判別
 						# ↓コマンドの引数の分解 タスクの名前、科目、締め切りをそれぞれの変数に代入(締め切りは前にある時刻の整形関数に通されてから代入)
 						task_name = command_list[0]
 						subject = command_list[1]
@@ -261,11 +265,11 @@ def list_process(message, on_cmd_cnl):
 		else:
 			cmd_chl = False
 			rtn_msg = random.choice(Wrong_channel)
-	elif '/remove' in command: # /remove がメッセージ内に入っているかの判別
+	elif '/remove' in command:  # /remove がメッセージ内に入っているかの判別
 		cmd_chl = True
 		if on_cmd_cnl:
-			command_list = command.split()[1:] # コマンドをスペースで区切り、/removeだけ消す(例：/remove task → task)
-			if len(command_list) == 1: # コマンドが正しい形かどうか判別
+			command_list = command.split()[1:]  # コマンドをスペースで区切り、/removeだけ消す(例：/remove task → task)
+			if len(command_list) == 1:  # コマンドが正しい形かどうか判別
 				counter = 0
 				detect = False
 				# 引数に該当するタスク名を課題リストから検索、あったら変数detectを真にし、場所を変数counterに代入
@@ -294,11 +298,11 @@ def list_process(message, on_cmd_cnl):
 		else:
 			cmd_chl = False
 			rtn_msg = random.choice(Wrong_channel)
-	elif '/rename' in command: # /remove がメッセージ内に入っているかの判別
+	elif '/rename' in command:  # /remove がメッセージ内に入っているかの判別
 		cmd_chl = True
 		if on_cmd_cnl:
-			command_list = command.split()[1:] # コマンドをスペースで区切り、/removeだけ消す(例：/remove task → task)
-			if len(command_list) == 2: # コマンドが正しい形かどうか判別
+			command_list = command.split()[1:]  # コマンドをスペースで区切り、/removeだけ消す(例：/remove task → task)
+			if len(command_list) == 2:  # コマンドが正しい形かどうか判別
 				counter = 0
 				detect = False
 				# 引数に該当するタスク名を課題リストから検索、あったら変数detectを真にし、場所を変数counterに代入
@@ -308,13 +312,11 @@ def list_process(message, on_cmd_cnl):
 						break
 					counter = counter + 1
 				if detect:
-					counter_2 = 0
 					double = False
 					for i in remind_list:
 						if command_list[1] == i[1]:
 							double = True
 							break
-						counter = counter + 1
 					if not double:
 						# あった場合、改名し、改名したタスク名を変数(task)に代入
 						task = remind_list[counter][1]
@@ -414,6 +416,7 @@ command_channel = client.get_channel(BOT_COMMAND_CHANNEL)
 data_channel = client.get_channel(BOT_DATA_CHANNEL)
 change = False
 
+
 # 起動時に動作する処理
 @client.event
 async def on_ready():
@@ -452,10 +455,11 @@ async def on_message(message):
 		# 先ほどのコマンドを解釈し、実行する関数にメッセージ内容を入れて実行し、返り値rtn_msgを得る
 		rtn_msg, cmd_cnl = list_process(message, on_cmd_cnl)
 		if cmd_cnl:
-			if rtn_msg: # rtn_msgに何か書いていたらそれをコマンド送信用チャンネルに送信
+			if rtn_msg:  # rtn_msgに何か書いていたらそれをコマンド送信用チャンネルに送信
 				remind_list = sorted(remind_list)
 				sndmsg = list_show(remind_list, option='normal')
 				data_channel = client.get_channel(BOT_DATA_CHANNEL)
+				
 				def is_me(m):
 					return m.author == client.user
 				await data_channel.purge(limit=100)
@@ -468,10 +472,10 @@ async def on_message(message):
 		else:
 			if rtn_msg:
 				await message.channel.send(rtn_msg)
-	except: # 何かエラーが起きたらその内容をbotlogチャンネルに送信
+	except:  # 何かエラーが起きたらその内容をbotlogチャンネルに送信
 		log_channel = client.get_channel(BOT_LOG_CHANNEL)
 		exc_type, exc_obj, exc_tb = sys.exc_info()
-		await log_channel.send('Error in line ' + str( exc_tb.tb_lineno ) + ' in ' +str(os.path.split( exc_tb.tb_frame.f_code.co_filename )[ 1 ]))
+		await log_channel.send('Error in line ' + str(exc_tb.tb_lineno) + ' in ' +　str(os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]))
 		await log_channel.send(str(sys.exc_info()[0]))
 		await log_channel.send(str(sys.exc_info()[1]))
 		await log_channel.send(str(sys.exc_info()[2]))
